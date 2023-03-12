d30ab0e9368e511d0fdec5ce26ac1b2545576c29
OttoCarius <ottocarius1993@gmail.com>
2023-03-12 18:19:27 +0200

    fx

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter
-    basename="yum-yum-recipes
-"
-  >
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
